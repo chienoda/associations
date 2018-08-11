1dea611f7d034471cc5dbc51e5d879a5f5d6bab7
Chie Noda <chienoda119@gmail/com>
2018-08-11 19:26:31 +0000

    Clean up the code with module.exports

--- references.js
+++ references.js
@@ -1,25 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/blog_demo_2', { useNewUrlParser: true });
-
-//POST - title, content
-var postSchema = new mongoose.Schema({
-   title: String,
-   content: String
-});
-var Post = mongoose.model('Post', postSchema);
-
-// USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }
-    ]
-});
-var User = mongoose.model('User', userSchema);
+var Post = require('./models/post');
+var User = require('./models/user');
 
 // User.create({
 //     email: 'bob@gmail.com',
@@ -47,7 +29,7 @@ var User = mongoose.model('User', userSchema);
 // });
 
 // Post.create({
-//     title: 'How to cook the best burger pt. 3',
+//     title: 'How to cook the best burger pt. 5',
 //     content: 'kokokokokokokokokokokokoko'
 // }, function(err, post){
 //     User.findOne({email: 'bob@gmail.com'}, function(err, foundUser){
@@ -68,10 +50,11 @@ var User = mongoose.model('User', userSchema);
 
 // Find user
 // find all posts for that user
-User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(user);
-    }
-});
+// User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
+
